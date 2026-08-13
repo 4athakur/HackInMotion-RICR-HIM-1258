622d34d9531a6b81bc91ef6e1fc03abdc142ae86
Aarushi123Khandelwal <aarushi11khandelwal@gmail.com>
2026-08-13 20:13:01 +0530

    Add budget data interfaces

--- src/pages/Budgets.tsx
+++ src/pages/Budgets.tsx
@@ -9,3 +9,45 @@ import {
 import * as Progress from '@radix-ui/react-progress';
 import clsx from 'clsx';
 import ReactMarkdown from 'react-markdown';
+interface CategoryItem {
+  id: number;
+  name: string;
+  icon: string;
+  color: string;
+}
+
+interface BudgetItem {
+  id: number;
+  categoryId: number;
+  categoryName: string;
+  categoryIcon: string;
+  categoryColor: string;
+  amount: number;
+  spent: number;
+  remaining: number;
+  percentageUsed: number;
+  status: 'normal' | 'near_limit' | 'exceeded';
+  daysPassed: number;
+  daysInMonth: number;
+  daysRemaining: number;
+  dailyBurnRate: number;
+  projectedSpend: number;
+  projectedOverrun: number;
+  isPredictedOverrun: boolean;
+  targetDailyRateForRemaining: number;
+  aiRecommendation: string;
+}
+
+interface SummaryData {
+  totalBudget: number;
+  totalSpent: number;
+  totalRemaining: number;
+  overallPercentage: number;
+  nearLimitCount: number;
+  predictedOverrunCount: number;
+  exceededCount: number;
+  daysPassed: number;
+  daysInMonth: number;
+  daysRemaining: number;
+  overallAiAdvice: string;
+}
